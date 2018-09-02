6480a8bc928a461e080d607b3904699ac5539451
IssingZhanggit config --global user.email 1193046858@qq.comgit config --global user.name IssingZhang <1193046858@qq.com>
2018-09-02 22:32:08 +0800

    add lib/

--- meadowlark.js
+++ meadowlark.js
@@ -1,16 +1,9 @@
 var express = require('express');
-
 var app = express();
 
 var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
 
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple."
-];
+var fortune = require('./lib/fortune.js');
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -24,8 +17,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/about', function (req, res) {
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune: randomFortune});
+    res.render('about', {fortune: fortune.getFortune()});
 });
 
 // 定制 404 页面
