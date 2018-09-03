b35fbb7eed108797c47181a91bf39e548cb243f3
IssingZhanggit config --global user.email 1193046858@qq.comgit config --global user.name IssingZhang <1193046858@qq.com>
2018-09-03 23:36:42 +0800

    add test

--- meadowlark.js
+++ meadowlark.js
@@ -10,6 +10,11 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(function(req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
