1136ac332d98920bc6f795049f7e807c7524643f
IssingZhanggit config --global user.email 1193046858@qq.comgit config --global user.name IssingZhang <1193046858@qq.com>
2018-09-06 22:36:21 +0800

    modified QA

--- meadowlark.js
+++ meadowlark.js
@@ -22,7 +22,19 @@ app.get('/', function (req, res) {
 });
 
 app.get('/about', function (req, res) {
-    res.render('about', {fortune: fortune.getFortune()});
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    });
+});
+
+app.get('/tours/hood-river', function(req, res) {
+    res.render('tours/hood-river');
+});
+
+
+app.get('/tours/request-group-rate', function(req, res) {
+    res.render('tours/request-group-rate');
 });
 
 // 定制 404 页面
